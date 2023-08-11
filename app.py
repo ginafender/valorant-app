f4ef78ff7381adb17a0d188b8c4d4700726f8a51
ginafender <123966430+ginafender@users.noreply.github.com>
2023-08-11 11:46:28 -0400

    added maps to flask app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,8 +10,96 @@ def home():
     print("Server received request for 'Home' page...")
     return "Welcome to my 'Home' page!"
 
+#######################################################################
+#################### ASCENT ###########################################
+
+@app.route("/ascent")
+def ascent():
+    with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
+        data = json.load(json_file)
+    image_path  = data['data'][0]['displayIcon']
+    return render_template('index.html', image_path=image_path)
+
+#######################################################################
+#################### SPLIT ############################################
+
+@app.route("/split")
+def split():
+    with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
+        data = json.load(json_file)
+    image_path  = data['data'][1]['displayIcon']
+    return render_template('index.html', image_path=image_path)
+
+#######################################################################
+#################### FRACTURE #########################################
+
+@app.route("/fracture")
+def fracture():
+    with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
+        data = json.load(json_file)
+    image_path  = data['data'][2]['displayIcon']
+    return render_template('index.html', image_path=image_path)
+
+#######################################################################
+#################### BIND #########################################
+
+@app.route("/bind")
+def bind():
+    with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
+        data = json.load(json_file)
+    image_path  = data['data'][3]['displayIcon']
+    return render_template('index.html', image_path=image_path)
+
+#######################################################################
+#################### BREEZE #########################################
+
+@app.route("/breeze")
+def breeze():
+    with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
+        data = json.load(json_file)
+    image_path  = data['data'][4]['displayIcon']
+    return render_template('index.html', image_path=image_path)
 
 
+#######################################################################
+#################### LOTUS #########################################
+
+@app.route("/lotus")
+def lotus():
+    with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
+        data = json.load(json_file)
+    image_path  = data['data'][8]['displayIcon']
+    return render_template('index.html', image_path=image_path)
+
+#######################################################################
+#################### PEARL #########################################
+
+@app.route("/pearl")
+def pearl():
+    with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
+        data = json.load(json_file)
+    image_path  = data['data'][9]['displayIcon']
+    return render_template('index.html', image_path=image_path)
+
+#######################################################################
+#################### ICEBOX #########################################
+
+@app.route("/icebox")
+def icebox():
+    with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
+        data = json.load(json_file)
+    image_path  = data['data'][10]['displayIcon']
+    return render_template('index.html', image_path=image_path)
+
+#######################################################################
+#################### HAVEN #########################################
+
+@app.route("/haven")
+def haven():
+    with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
+        data = json.load(json_file)
+    image_path  = data['data'][12]['displayIcon']
+    return render_template('index.html', image_path=image_path)
 
 if __name__ == "__main__":
     app.run(debug=True)
