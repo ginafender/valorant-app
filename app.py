a75bf98eeec735f1679ad029b5357a1a914d1c4e
ginafender <123966430+ginafender@users.noreply.github.com>
2023-08-28 15:13:10 -0400

    added all splash images and updated html for each page

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+app.static_folder = 'static'
 
 
 # 3. Define what to do when a user hits the index route
@@ -66,8 +67,46 @@ def ascent():
 def split():
     with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
         maps = json.load(json_file)
-    image_path  = maps['data'][1]['displayIcon']
-    return render_template('split.html', image_path=image_path)
+    with open("..//valorant-app/json/valorantmatches.json", "r") as map_file:
+        match_data = json.load(map_file)
+
+    mapURL = '/Game/Maps/Bonsai/Bonsai'
+    death_coordinates = []
+    round_results = []
+
+    matches = match_data['matches']
+    # Iterate through rounds and their results
+    for match in matches:
+        if match['matchInfo']['mapId'] == mapURL:
+            round_results = match['roundResults']
+
+        # Iterate through player stats for each round
+        for round_result in round_results:
+            for player_stat in round_result['playerStats']:
+                kills = player_stat['kills']
+
+                # Iterate through kills for each player
+                for kill in kills:
+                    if 'victimLocation' in kill:
+                        victim_location = kill['victimLocation']
+                        death_coordinates.append(victim_location)
+
+    map_data = maps['data'][1]
+    x_Multiplier = map_data['xMultiplier']
+    y_Multiplier = map_data['yMultiplier']
+    x_ScalarToAdd = map_data['xScalarToAdd']
+    y_ScalarToAdd = map_data['yScalarToAdd']
+
+    mini_map_coordinates = []
+    for death in death_coordinates:
+        mini_x = death['y'] * x_Multiplier + x_ScalarToAdd
+        mini_y = death['x'] * y_Multiplier + y_ScalarToAdd
+        mini_map_coordinates.append({'x': mini_x, 'y': mini_y})
+
+    image_path  = "valorant-app/static/map_images/split_image.png"
+    return render_template('split.html', image_path=image_path, mini_map_coordinates=mini_map_coordinates,
+                            x_Multiplier=x_Multiplier, y_Multiplier=y_Multiplier, x_ScalarToAdd=x_ScalarToAdd,
+                            y_ScalarToAdd=y_ScalarToAdd)
 
 #######################################################################
 #################### FRACTURE #########################################
@@ -123,8 +162,46 @@ def fracture():
 def bind():
     with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
         maps = json.load(json_file)
-    image_path  = maps['data'][3]['displayIcon']
-    return render_template('bind.html', image_path=image_path)
+    with open("..//valorant-app/json/valorantmatches.json", "r") as map_file:
+        match_data = json.load(map_file)
+
+    mapURL = "/Game/Maps/Duality/Duality"
+    death_coordinates = []
+    round_results = []
+
+    matches = match_data['matches']
+    # Iterate through rounds and their results
+    for match in matches:
+        if match['matchInfo']['mapId'] == mapURL:
+            round_results = match['roundResults']
+
+        # Iterate through player stats for each round
+        for round_result in round_results:
+            for player_stat in round_result['playerStats']:
+                kills = player_stat['kills']
+
+                # Iterate through kills for each player
+                for kill in kills:
+                    if 'victimLocation' in kill:
+                        victim_location = kill['victimLocation']
+                        death_coordinates.append(victim_location)
+
+    map_data = maps['data'][3]
+    x_Multiplier = map_data['xMultiplier']
+    y_Multiplier = map_data['yMultiplier']
+    x_ScalarToAdd = map_data['xScalarToAdd']
+    y_ScalarToAdd = map_data['yScalarToAdd']
+
+    mini_map_coordinates = []
+    for death in death_coordinates:
+        mini_x = death['y'] * x_Multiplier + x_ScalarToAdd
+        mini_y = death['x'] * y_Multiplier + y_ScalarToAdd
+        mini_map_coordinates.append({'x': mini_x, 'y': mini_y})
+
+    image_path  = "valorant-app/static/map_images/bind_image.png"
+    return render_template('bind.html', image_path=image_path, mini_map_coordinates=mini_map_coordinates,
+                            x_Multiplier=x_Multiplier, y_Multiplier=y_Multiplier, x_ScalarToAdd=x_ScalarToAdd,
+                            y_ScalarToAdd=y_ScalarToAdd)
 
 #######################################################################
 #################### BREEZE ###########################################
@@ -133,8 +210,46 @@ def bind():
 def breeze():
     with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
         maps = json.load(json_file)
-    image_path  = maps['data'][4]['displayIcon']
-    return render_template('breeze.html', image_path=image_path)
+    with open("..//valorant-app/json/valorantmatches.json", "r") as map_file:
+        match_data = json.load(map_file)
+
+    mapURL = "/Game/Maps/Foxtrot/Foxtrot"
+    death_coordinates = []
+    round_results = []
+
+    matches = match_data['matches']
+    # Iterate through rounds and their results
+    for match in matches:
+        if match['matchInfo']['mapId'] == mapURL:
+            round_results = match['roundResults']
+
+        # Iterate through player stats for each round
+        for round_result in round_results:
+            for player_stat in round_result['playerStats']:
+                kills = player_stat['kills']
+
+                # Iterate through kills for each player
+                for kill in kills:
+                    if 'victimLocation' in kill:
+                        victim_location = kill['victimLocation']
+                        death_coordinates.append(victim_location)
+
+    map_data = maps['data'][4]
+    x_Multiplier = map_data['xMultiplier']
+    y_Multiplier = map_data['yMultiplier']
+    x_ScalarToAdd = map_data['xScalarToAdd']
+    y_ScalarToAdd = map_data['yScalarToAdd']
+
+    mini_map_coordinates = []
+    for death in death_coordinates:
+        mini_x = death['y'] * x_Multiplier + x_ScalarToAdd
+        mini_y = death['x'] * y_Multiplier + y_ScalarToAdd
+        mini_map_coordinates.append({'x': mini_x, 'y': mini_y})
+
+    image_path  = "valorant-app/static/map_images/breeze_image.png"
+    return render_template('breeze.html', image_path=image_path, mini_map_coordinates=mini_map_coordinates,
+                            x_Multiplier=x_Multiplier, y_Multiplier=y_Multiplier, x_ScalarToAdd=x_ScalarToAdd,
+                            y_ScalarToAdd=y_ScalarToAdd)
 
 
 #######################################################################
@@ -144,8 +259,46 @@ def breeze():
 def lotus():
     with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
         maps = json.load(json_file)
-    image_path  = maps['data'][8]['displayIcon']
-    return render_template('lotus.html', image_path=image_path)
+    with open("..//valorant-app/json/valorantmatches.json", "r") as map_file:
+        match_data = json.load(map_file)
+
+    mapURL = "/Game/Maps/Jam/Jam"
+    death_coordinates = []
+    round_results = []
+
+    matches = match_data['matches']
+    # Iterate through rounds and their results
+    for match in matches:
+        if match['matchInfo']['mapId'] == mapURL:
+            round_results = match['roundResults']
+
+        # Iterate through player stats for each round
+        for round_result in round_results:
+            for player_stat in round_result['playerStats']:
+                kills = player_stat['kills']
+
+                # Iterate through kills for each player
+                for kill in kills:
+                    if 'victimLocation' in kill:
+                        victim_location = kill['victimLocation']
+                        death_coordinates.append(victim_location)
+
+    map_data = maps['data'][8]
+    x_Multiplier = map_data['xMultiplier']
+    y_Multiplier = map_data['yMultiplier']
+    x_ScalarToAdd = map_data['xScalarToAdd']
+    y_ScalarToAdd = map_data['yScalarToAdd']
+
+    mini_map_coordinates = []
+    for death in death_coordinates:
+        mini_x = death['y'] * x_Multiplier + x_ScalarToAdd
+        mini_y = death['x'] * y_Multiplier + y_ScalarToAdd
+        mini_map_coordinates.append({'x': mini_x, 'y': mini_y})
+
+    image_path  = "valorant-app/static/map_images/lotus_image.png"
+    return render_template('lotus.html', image_path=image_path, mini_map_coordinates=mini_map_coordinates,
+                            x_Multiplier=x_Multiplier, y_Multiplier=y_Multiplier, x_ScalarToAdd=x_ScalarToAdd,
+                            y_ScalarToAdd=y_ScalarToAdd)
 
 #######################################################################
 #################### PEARL ############################################
@@ -154,8 +307,46 @@ def lotus():
 def pearl():
     with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
         maps = json.load(json_file)
-    image_path  = maps['data'][9]['displayIcon']
-    return render_template('pearl.html', image_path=image_path)
+    with open("..//valorant-app/json/valorantmatches.json", "r") as map_file:
+        match_data = json.load(map_file)
+
+    mapURL = "/Game/Maps/Pitt/Pitt"
+    death_coordinates = []
+    round_results = []
+
+    matches = match_data['matches']
+    # Iterate through rounds and their results
+    for match in matches:
+        if match['matchInfo']['mapId'] == mapURL:
+            round_results = match['roundResults']
+
+        # Iterate through player stats for each round
+        for round_result in round_results:
+            for player_stat in round_result['playerStats']:
+                kills = player_stat['kills']
+
+                # Iterate through kills for each player
+                for kill in kills:
+                    if 'victimLocation' in kill:
+                        victim_location = kill['victimLocation']
+                        death_coordinates.append(victim_location)
+
+    map_data = maps['data'][9]
+    x_Multiplier = map_data['xMultiplier']
+    y_Multiplier = map_data['yMultiplier']
+    x_ScalarToAdd = map_data['xScalarToAdd']
+    y_ScalarToAdd = map_data['yScalarToAdd']
+
+    mini_map_coordinates = []
+    for death in death_coordinates:
+        mini_x = death['y'] * x_Multiplier + x_ScalarToAdd
+        mini_y = death['x'] * y_Multiplier + y_ScalarToAdd
+        mini_map_coordinates.append({'x': mini_x, 'y': mini_y})
+
+    image_path  = "valorant-app/static/map_images/pearl_image.png"
+    return render_template('pearl.html', image_path=image_path, mini_map_coordinates=mini_map_coordinates,
+                            x_Multiplier=x_Multiplier, y_Multiplier=y_Multiplier, x_ScalarToAdd=x_ScalarToAdd,
+                            y_ScalarToAdd=y_ScalarToAdd)
 
 #######################################################################
 #################### ICEBOX ###########################################
@@ -164,8 +355,46 @@ def pearl():
 def icebox():
     with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
         maps = json.load(json_file)
-    image_path  = maps['data'][10]['displayIcon']
-    return render_template('icebox.html', image_path=image_path)
+    with open("..//valorant-app/json/valorantmatches.json", "r") as map_file:
+        match_data = json.load(map_file)
+
+    mapURL = "/Game/Maps/Port/Port"
+    death_coordinates = []
+    round_results = []
+
+    matches = match_data['matches']
+    # Iterate through rounds and their results
+    for match in matches:
+        if match['matchInfo']['mapId'] == mapURL:
+            round_results = match['roundResults']
+
+        # Iterate through player stats for each round
+        for round_result in round_results:
+            for player_stat in round_result['playerStats']:
+                kills = player_stat['kills']
+
+                # Iterate through kills for each player
+                for kill in kills:
+                    if 'victimLocation' in kill:
+                        victim_location = kill['victimLocation']
+                        death_coordinates.append(victim_location)
+
+    map_data = maps['data'][10]
+    x_Multiplier = map_data['xMultiplier']
+    y_Multiplier = map_data['yMultiplier']
+    x_ScalarToAdd = map_data['xScalarToAdd']
+    y_ScalarToAdd = map_data['yScalarToAdd']
+
+    mini_map_coordinates = []
+    for death in death_coordinates:
+        mini_x = death['y'] * x_Multiplier + x_ScalarToAdd
+        mini_y = death['x'] * y_Multiplier + y_ScalarToAdd
+        mini_map_coordinates.append({'x': mini_x, 'y': mini_y})
+
+    image_path  = "valorant-app/static/map_images/icebox_image.png"
+    return render_template('icebox.html', image_path=image_path, mini_map_coordinates=mini_map_coordinates,
+                            x_Multiplier=x_Multiplier, y_Multiplier=y_Multiplier, x_ScalarToAdd=x_ScalarToAdd,
+                            y_ScalarToAdd=y_ScalarToAdd)
 
 #######################################################################
 #################### HAVEN ############################################
@@ -174,8 +403,46 @@ def icebox():
 def haven():
     with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
         maps = json.load(json_file)
-    image_path  = maps['data'][12]['displayIcon']
-    return render_template('haven.html', image_path=image_path)
+    with open("..//valorant-app/json/valorantmatches.json", "r") as map_file:
+        match_data = json.load(map_file)
+
+    mapURL = "/Game/Maps/Triad/Triad"
+    death_coordinates = []
+    round_results = []
+
+    matches = match_data['matches']
+    # Iterate through rounds and their results
+    for match in matches:
+        if match['matchInfo']['mapId'] == mapURL:
+            round_results = match['roundResults']
+
+        # Iterate through player stats for each round
+        for round_result in round_results:
+            for player_stat in round_result['playerStats']:
+                kills = player_stat['kills']
+
+                # Iterate through kills for each player
+                for kill in kills:
+                    if 'victimLocation' in kill:
+                        victim_location = kill['victimLocation']
+                        death_coordinates.append(victim_location)
+
+    map_data = maps['data'][12]
+    x_Multiplier = map_data['xMultiplier']
+    y_Multiplier = map_data['yMultiplier']
+    x_ScalarToAdd = map_data['xScalarToAdd']
+    y_ScalarToAdd = map_data['yScalarToAdd']
+
+    mini_map_coordinates = []
+    for death in death_coordinates:
+        mini_x = death['y'] * x_Multiplier + x_ScalarToAdd
+        mini_y = death['x'] * y_Multiplier + y_ScalarToAdd
+        mini_map_coordinates.append({'x': mini_x, 'y': mini_y})
+
+    image_path  = "valorant-app/static/map_images/haven_image.png"
+    return render_template('haven.html', image_path=image_path, mini_map_coordinates=mini_map_coordinates,
+                            x_Multiplier=x_Multiplier, y_Multiplier=y_Multiplier, x_ScalarToAdd=x_ScalarToAdd,
+                            y_ScalarToAdd=y_ScalarToAdd)
 
 if __name__ == "__main__":
     app.run(debug=True)
