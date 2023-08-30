6b019162b96a4f51315fed4f6ec1ea30bf8f4c97
ginafender <123966430+ginafender@users.noreply.github.com>
2023-08-29 20:50:52 -0400

    added/updated js file, testing code on ascent.html file

--- static/js/app.js
+++ static/js/app.js
@@ -3,13 +3,46 @@ const map_url = "https://valorant-api.com/v1/maps";
 const character_url = "https://valorant-api.com/v1/agents";
 
 // Get all death coordinates
-function deathCoordinates(deaths) {
+function deathCoordinates(mapName) {
     Promise.all([
         d3.json(match_url),
         d3.json(map_url)
     ]).then(function([data01, data02]){
-        let matches = data01.matches;
-        let mapId = matches.mapId;
-        
+        let matches = data01.data.matches;
+        let mapId = data02.data.find(map => map.displayName === mapName).uuid;
+        let deathsLocations = [];
+
+        for (const match of matches) {
+            // Iterate through rounds and their results
+            if (match.matchInfo.mapId === mapId){ 
+                let roundResults = match.roundResults;
+
+                // Iterate through player stats for each round
+                for (const roundResult of roundResults) {
+                    for (const player_stat of roundResult.playerStats) {
+                        const kills = playerStat.kills;
+                        
+                    // Iterate through kills for each player
+                        for (const kill of kills) {
+                            if ('victimLocation' in kill) {
+                                const victim_location = kill.victimLocation;
+                                deathsLocations.push(victim_location);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        const xMulti = data02.data.xMultiplier;
+        const xScalar = data02.data.xScalarToAdd;
+        const yMulti = data02.data.yMultiplier;
+        const yScalar = data02.data.yScalarToAdd;
+
+        const mini_map_coordinates = [];
+        for (const death of deathsLocations) {
+            const mini_x = death.y * xMulti + xScalar;
+            const mini_y = death.x * yMulti + yScalar;
+            mini_map_coordinates.push({ 'x': mini_x, 'y': mini_y });
+        }
     });
-};
+}
