363ad14e57bd2243fc3091bca6011c9260b2c129
ginafender <123966430+ginafender@users.noreply.github.com>
2023-09-03 11:19:02 -0400

    updated structure

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ Flask app for each individual map.
 """
 
 import json
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -14,26 +14,23 @@ def home():
     print("Server received request for 'Home' page...")
     return render_template('index.html')
 # ----------------------- ASCENT -----------------------
-@app.route("/ascent", methods=['GET', 'POST'])
+@app.route("/ascent")
 def ascent():
+    selected_tier = request.args.get('tier')  # Get the selected tier from request args
+
     with open("..//valorant-app/json/valorantmaps.json", "r") as json_file:
         maps = json.load(json_file)
     with open("..//valorant-app/json/valorantmatches.json", "r") as map_file:
         match_data = json.load(map_file)
 
-    map_data = maps['data'][0]
-    x_Multiplier = map_data['xMultiplier']
-    y_Multiplier = map_data['yMultiplier']
-    x_ScalarToAdd = map_data['xScalarToAdd']
-    y_ScalarToAdd = map_data['yScalarToAdd']
-
     mapURL = '/Game/Maps/Ascent/Ascent'
     death_coordinates = []
     round_results = []
 
-    matches = match_data['matches']
-    mini_map_coordinates = []  # Define mini_map_coordinates here
+    # Create a dictionary to map player puuids to competitive tiers
+    player_tiers_dict = {}
 
+    matches = match_data['matches']
     # Iterate through rounds and their results
     for match in matches:
         if match['matchInfo']['mapId'] == mapURL:
@@ -48,72 +45,43 @@ def ascent():
                 for kill in kills:
                     if 'victimLocation' in kill:
                         victim_location = kill['victimLocation']
-                        victim_id = kill['victim']  # Get the victim's PUUID
 
-                        # Iterate through players to find the competitive tier
-                        for player_data in match['players']:
-                            if player_data['puuid'] == victim_id:
-                                player_tier = player_data['competitiveTier'].lower()
+                        # Check if the competitive tier matches the selected tier
+                        player_puuid = player_stat['puuid']
+                        player_tier = None
+                        
+                        # Find the player's tier based on their puuid
+                        for player in match['players']:
+                            if player['puuid'] == player_puuid:
+                                player_tier = player['competitiveTier']
                                 break
 
-                        # Now you have the victim's coordinates and competitive tier
-                        mini_x = victim_location['y'] * x_Multiplier + x_ScalarToAdd
-                        mini_y = victim_location['x'] * y_Multiplier + y_ScalarToAdd
+                        # Add the player's tier to the dictionary
+                        player_tiers_dict[player_puuid] = player_tier
 
-                        # Append the coordinate with competitive tier
-                        mini_map_coordinates.append({'x': mini_x, 'y': mini_y, 'victim_id': victim_id, 'competitiveTier': player_tier})
+                        # Filter by competitive tier
+                        if selected_tier is None or player_tier == selected_tier:
+                            death_coordinates.append(victim_location)
 
-    print('mini map cords: ', mini_map_coordinates)
+    map_data = maps['data'][0]
+    x_Multiplier = map_data['xMultiplier']
+    y_Multiplier = map_data['yMultiplier']
+    x_ScalarToAdd = map_data['xScalarToAdd']
+    y_ScalarToAdd = map_data['yScalarToAdd']
 
-    if request.method == 'POST':
-        selected_tier = request.form.get('selected_tier')
-        print('Selected tier:', selected_tier)
-        if not selected_tier:
-            selected_tier = 'UNRANKED'
-        if selected_tier:
-            selected_tier_lower = selected_tier.lower()
-            filtered_mini_map_coordinates = []
+    mini_map_coordinates = []
+    for death in death_coordinates:
+        mini_x = death['y'] * x_Multiplier + x_ScalarToAdd
+        mini_y = death['x'] * y_Multiplier + y_ScalarToAdd
+        mini_map_coordinates.append({'x': mini_x, 'y': mini_y})
 
-            for coordinate in mini_map_coordinates:
-                player_tier = None
+    image_path  = "valorant-app/static/map_images/ascent_image.png"
 
-                # Find the player's competitive tier using victim_id
-                if 'victim_id' in coordinate:  # Check if 'victim_id' key exists
-                    for match in matches:
-                        for player_data in match['players']:
-                            if player_data.get('puuid') == coordinate['victim_id']:  # Use get() to avoid KeyError
-                                player_tier = player_data.get('competitiveTier', '').lower()  # Use get() to avoid KeyError
-                                break
-
-                if player_tier and selected_tier_lower in player_tier:
-                    filtered_mini_map_coordinates.append(coordinate)
-                else:
-                    print('Filtered out coordinate:', coordinate)
-
-            print('Filtered coordinates:', filtered_mini_map_coordinates)
-
-            # Serialize filtered_mini_map_coordinates as JSON
-            filtered_mini_map_coordinates = []
-
-            for coordinate in mini_map_coordinates:
-                # Check if the coordinate is serializable (exclude non-serializable data)
-                if all(isinstance(coordinate[key], (int, float, str)) for key in coordinate):
-                    filtered_mini_map_coordinates.append(coordinate)
-
-            mini_map_coordinates_json = jsonify(mini_map_coordinates=filtered_mini_map_coordinates)
-
-    print('final map cords: ', mini_map_coordinates)
-    
-    # Serialize mini_map_coordinates as JSON
-    mini_map_coordinates_json = jsonify(mini_map_coordinates=mini_map_coordinates)
-    
-    # testData = "This is a test"
-    image_path  = "valorant-app/static/map_images/split_image.png"
-    
-    # Pass mini_map_coordinates_json to the template
-    return render_template('ascent.html', image_path=image_path, mini_map_coordinates_json=mini_map_coordinates_json,
+    # Pass player_tiers_dict instead of player_tier
+    return render_template('ascent.html', image_path=image_path, mini_map_coordinates=mini_map_coordinates,
                             x_Multiplier=x_Multiplier, y_Multiplier=y_Multiplier, x_ScalarToAdd=x_ScalarToAdd,
-                            y_ScalarToAdd=y_ScalarToAdd)
+                            y_ScalarToAdd=y_ScalarToAdd, player_tiers_dict=player_tiers_dict)
+
 # ----------------------- SPLIT -----------------------
 @app.route("/split")
 def split():
